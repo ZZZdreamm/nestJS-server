d57216c27ad65ab941314744f4855fdd87d435e3
ZZZdream <kmultan1234@gmail.com>
2023-09-18 00:25:48 +0200

    Remove firebase credentials

--- src/database/testSetup.ts
+++ src/database/testSetup.ts
@@ -2,18 +2,7 @@ import * as functionsTest from 'firebase-functions-test';
 import * as firebase from 'firebase-admin';
 import { getFirestore, connectFirestoreEmulator } from '@firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCncHs0oDoCmDveoAMZvSkLXuc4syN2Mg4',
-  authDomain: 'facebugserver.firebaseapp.com',
-  databaseURL:
-    'https://facebugserver-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'facebugserver',
-  storageBucket: 'facebugserver.appspot.com',
-  messagingSenderId: '915472090039',
-  appId: '1:915472090039:web:532f827244997362d29e32',
-};
 
-const test = functionsTest(firebaseConfig, 'socialAppFirebaseCredentials.json');
 
 beforeAll(() => {
   const emulatorHost = 'localhost';
@@ -25,8 +14,4 @@ beforeAll(() => {
   });
   const db = getFirestore();
   connectFirestoreEmulator(db, emulatorHost, emulatorPort);
-});
-
-afterAll(() => {
-  test.cleanup();
 });
