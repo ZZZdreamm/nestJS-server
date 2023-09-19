9cfd7958324a5f3c8a5126f2ace5ba6d96e0e6f1
ZZZdream <kmultan1234@gmail.com>
2023-09-19 11:09:37 +0200

    Import modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,12 @@ import { ProfilesModule } from './profiles/profiles.module';
 import { PostsController } from './posts/posts.controller';
 import { PostsService } from './posts/posts.service';
 import { PostsModule } from './posts/posts.module';
+import { CommentsController } from './comments/comments.controller';
+import { CommentsService } from './comments/comments.service';
+import { CommentsModule } from './comments/comments.module';
+import { MessagesController } from './messages/messages.controller';
+import { MessagesService } from './messages/messages.service';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
@@ -14,9 +20,16 @@ import { PostsModule } from './posts/posts.module';
     }),
     ProfilesModule,
     PostsModule,
+    CommentsModule,
+    MessagesModule,
   ],
   exports: [FirebaseModule],
-  controllers: [ProfilesController, PostsController],
-  providers: [ProfilesService, PostsService],
+  controllers: [
+    ProfilesController,
+    PostsController,
+    CommentsController,
+    MessagesController,
+  ],
+  providers: [ProfilesService, PostsService, CommentsService, MessagesService],
 })
 export class AppModule {}
