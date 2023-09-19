875c5278b99a9020b630b72edaa88048b90c11ed
ZZZdream <kmultan1234@gmail.com>
2023-09-19 11:06:58 +0200

    Add functions to posts service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,8 +1,9 @@
 import { Inject, Injectable, forwardRef } from '@nestjs/common';
 import { FirebaseAdmin, InjectFirebaseAdmin } from 'nestjs-firebase';
-import { Post } from './entities/post.entity';
 import { PostDto } from './dto/postDto';
 import { ProfilesService } from 'src/profiles/profiles.service';
+import { PostCreateDto } from './dto/postCreateDto';
+import { UpdatePostDto } from './dto/updatePostDto';
 
 @Injectable()
 export class PostsService {
@@ -11,8 +12,20 @@ export class PostsService {
     @Inject(forwardRef(() => ProfilesService))
     private readonly profilesService: ProfilesService,
   ) {}
+
+  async create(post: PostCreateDto) {
+    const fullPost = {
+      AmountOfComments: 0,
+      AmountOfLikes: 0,
+      ...post,
+    };
+    const postsCollection = this.firebase.firestore.collection('Posts');
+    const newPost = await postsCollection.add(fullPost);
+    const autorImage = await this.profilesService.getUserImage(post.AutorName);
+    return { Id: newPost.id, AutorProfileImage: autorImage, ...fullPost };
+  }
+
   async getSomeNewest(numberOfPosts: number) {
-    console.log(numberOfPosts);
     const postsCollection = this.firebase.firestore.collection('Posts');
 
     const query = postsCollection.limit(numberOfPosts);
@@ -35,5 +48,108 @@ export class PostsService {
         posts = await Promise.all(promises);
       });
     return posts;
+  }
+
+  async likePost(postId: string, userId: string) {
+    const postRef = this.firebase.firestore.collection('Posts').doc(postId);
+    const ifAlreadyLiked = await postRef
+      .collection('Likes')
+      .where('UserId', '==', userId)
+      .get()
+      .then((querySnapshot) => {
+        if (querySnapshot.docs.length > 0) {
+          return true;
+        }
+      });
+    if (ifAlreadyLiked) return false;
+    await postRef.collection('Likes').add({ UserId: userId });
+    await postRef.get().then(async (doc) => {
+      const likes = await doc.data()?.AmountOfLikes;
+      await postRef.update({
+        AmountOfLikes: likes + 1,
+      });
+    });
+    return true;
+  }
+
+  async removeLike(postId: string, userId: string) {
+    const postRef = this.firebase.firestore.collection('Posts').doc(postId);
+    let deleted = false;
+    const docs = await postRef
+      .collection('Likes')
+      .where('UserId', '==', userId)
+      .get();
+    docs.forEach((doc) => {
+      doc.ref.delete();
+      deleted = true;
+    });
+    return deleted;
+  }
+
+  async ifUserLiked(postId: string, userId: string) {
+    const likesRef = this.firebase.firestore
+      .collection('Posts')
+      .doc(postId)
+      .collection('Likes');
+    let liked = false;
+    const docs = await likesRef.where('UserId', '==', userId).get();
+    docs.forEach((doc) => {
+      const data = doc.data();
+      if (data.UserId == userId) {
+        liked = true;
+      }
+    });
+
+    return liked;
+  }
+
+  async getPostsOfUser(username: string, numberOfPosts: number) {
+    const postsRef = this.firebase.firestore.collection('Posts');
+    const query = postsRef
+      .where('AutorName', '==', username)
+      .limit(numberOfPosts);
+    let posts: PostDto[] = [];
+    const querySnapshot = await query.orderBy('Date', 'desc').get();
+
+    const promises = querySnapshot.docs.map(async (doc) => {
+      const docData = doc.data();
+      const autor = await this.profilesService.getProfile(docData.AutorId);
+      let post: PostDto = {
+        AutorProfileImage: autor.ProfileImage,
+        AutorName: autor.Email,
+        Date: docData.Date,
+        MediaFiles: docData.MediaFiles,
+        Text: docData.Text,
+        AmountOfLikes: docData.AmountOfLikes,
+        AmountOfComments: docData.AmountOfComments,
+      };
+      return post;
+    });
+    posts = await Promise.all(promises);
+
+    return posts;
+  }
+
+  async deletePost(postId: string) {
+    try {
+      const postRef = this.firebase.firestore.collection('Posts').doc(postId);
+      await postRef.delete();
+    } catch (err) {
+      return 'error';
+    }
+    return 'success';
+  }
+
+  async updatePost(post: UpdatePostDto) {
+    try {
+      const postRef = this.firebase.firestore.collection('Posts').doc(post.Id);
+      await postRef.update({
+        TextContent: post.TextContent,
+        MediaFiles: post.MediaFiles,
+      });
+    } catch (err) {
+      return 'error';
+    }
+    return 'success';
   }
 }
