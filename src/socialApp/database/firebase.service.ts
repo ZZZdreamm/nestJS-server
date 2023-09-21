30226482f4aadb03802af81e9cfb90f177f342dc
ZZZdream <kmultan1234@gmail.com>
2023-09-21 13:47:40 +0200

    Test config fixes

--- src/socialApp/database/firebase.service.ts
+++ src/socialApp/database/firebase.service.ts
@@ -1,6 +1,7 @@
 import * as admin from 'firebase-admin';
 import { Injectable } from '@nestjs/common';
 import 'dotenv/config';
+import { getFirestore, connectFirestoreEmulator } from '@firebase/firestore';
 
 @Injectable()
 export class FirebaseService {
@@ -14,21 +15,34 @@ export class FirebaseService {
       }
     });
     if (!appInitilized) {
-      this.firestore = admin
-        .initializeApp(
-          {
-            credential: admin.credential.cert({
-              projectId: process.env.SOCIALAPP_PROJECT_ID,
-              clientEmail: process.env.SOCIALAPP_CLIENT_EMAIL,
-              privateKey: process.env.SOCIALAPP_PRIVATE_KEY.replace(
-                /\\n/g,
-                '\n',
-              ),
-            }),
-          },
-          'socialApp',
-        )
-        .firestore();
+      if (process.env.NODE_ENV === 'test') {
+        this.firestore = admin
+          .initializeApp(
+            {
+              credential: admin.credential.applicationDefault(),
+            },
+            'socialApp',
+          )
+          .firestore();
+        // const db = getFirestore();
+        // connectFirestoreEmulator(db, '127.0.0.1', 8080);
+      } else {
+        this.firestore = admin
+          .initializeApp(
+            {
+              credential: admin.credential.cert({
+                projectId: process.env.SOCIALAPP_PROJECT_ID,
+                clientEmail: process.env.SOCIALAPP_CLIENT_EMAIL,
+                privateKey: process.env.SOCIALAPP_PRIVATE_KEY.replace(
+                  /\\n/g,
+                  '\n',
+                ),
+              }),
+            },
+            'socialApp',
+          )
+          .firestore();
+      }
     } else {
       this.firestore = admin.app('socialApp').firestore();
     }
