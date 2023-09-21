8b0edd4e66ef48902f8010bd7681f2836e623793
ZZZdream <kmultan1234@gmail.com>
2023-09-21 13:27:55 +0200

    Add Modules and Services to CacarrotAppModule

--- src/cacarrot/cacarrotApp.module.ts
+++ src/cacarrot/cacarrotApp.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FirebaseService } from './database/firebase.service';
 import { FirebaseModule } from './database/firebase.module';
+import { WebsocketModule } from './gateways/websocket.module';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [FirebaseModule, WebsocketModule, UsersModule],
   exports: [FirebaseModule],
-  controllers: [],
-  providers: [FirebaseService],
+  controllers: [UsersController],
+  providers: [FirebaseService, UsersService],
 })
 export class CacarrotAppModule {}
